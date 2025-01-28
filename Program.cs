6eae7586a9ab7d099537de1669a41e18d1ea42e9
And-r-b <and.r.b@hotmail.com>
2025-01-28 10:50:47 +0100

    accept user input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Starting Renting Service");
+
+string? input = Console.ReadLine();
+if (input == null) 
+{
+    Environment.Exit(1);
+}
+
+Console.WriteLine("Echo: " + input);
